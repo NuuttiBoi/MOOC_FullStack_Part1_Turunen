98b5c7af01afaa41466aee10b51f72d2844b50a0
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2024-08-27 15:32:49 +0900

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 const StatisticLine = (props) => {
     return (
-        <div>
-            <p>{props.text} {props.value}</p>
-        </div>
+        <>
+            <tr>
+                <td>{props.text} {props.value}</td>
+            </tr>
+        </>
     )
 }
 
@@ -20,14 +22,16 @@ const Statistics = (props) => {
     if(props.all > 0) {
         return (
             <div>
-                <div>
+                <table>
+                    <tbody>
                     <StatisticLine text={"good"} value={props.good}/>
                     <StatisticLine text={"neutral"} value={props.neutral}/>
                     <StatisticLine text={"bad"} value={props.bad}/>
                     <StatisticLine text={"all"} value={props.all}/>
                     <StatisticLine text={"average"} value={props.average}/>
                     <StatisticLine text={"positive"} value={props.positive+"%"}/>
-                </div>
+                    </tbody>
+                </table>
             </div>
         )
     }
